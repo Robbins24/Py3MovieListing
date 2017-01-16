8beb081f63057f511dc1cc7167b86e768d44586b
SyntaxErrorz12 <nickrobbins.24@gmail.com>
2017-01-16 22:45:11 +0300

    Initial commit of Py3MovieDir
    Initial commit

---
+++ libs/html.py
@@ -0,0 +1,38 @@
+header = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Current Movie Directory</title>
+    
+    <link rel="stylesheet" href="foundation/css/foundation.min.css" />
+    <link rel="stylesheet" href="foundation/css/foundation-icons.css" />
+    <script src="foundation/js/vendor/modernizr.js"></script>
+    <script src="foundation/js/vendor/jquery.js"></script>
+	<script src="foundation/js/foundation/foundation.js"></script>    
+</head>
+
+<body>	
+	<div class="row">	
+      <div class="medium-12 columns">
+            <h2>Current Movie Directory</h2>
+			Movies with naming convention issues are still available without IMDB Data. They are listed at the bottom of this page.
+
+            <hr>
+
+        </div>
+    </div>
+    
+    
+'''
+
+footer = '''
+		
+
+        
+</body>
+</html>
+'''
+
+
+
